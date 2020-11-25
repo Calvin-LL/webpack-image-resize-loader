34275cbc1fa255f94f5a77f4660f1b1604ccb930
Calvin Liang <me@calvin.sh>
2020-11-25 12:57:10 -0800

    chore: add pre-push test to husky

--- husky.config.js
+++ husky.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "pre-commit": "lint-staged",
     "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+    "pre-push": "npm test",
   },
 };
