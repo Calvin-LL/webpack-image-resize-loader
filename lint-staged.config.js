37b0078f6e08c4f47be4999c983d2dd3a8983069
Calvin Liang <me@calvin.sh>
2020-11-29 16:00:27 -0800

    chore: fix remark in lint-staged exiting with 0 when there is a warning

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": "eslint --fix",
   "*.{js,jsx,ts,tsx,css,less,scss,vue,json,gql,md}": "prettier --write",
-  "*.{md}": "remark",
+  "*.{md}": "remark --frail",
 };
