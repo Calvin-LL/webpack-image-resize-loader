dc816c85ae6d941e4eb73e9d121eda728e5318f3
Calvin Liang <me@calvin.sh>
2020-12-13 14:27:17 -0800

    chore(examples): add example with query

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import image from "./assets/Macaca_nigra_self-portrait_large.jpg";
+import image100px from "./assets/Macaca_nigra_self-portrait_large.jpg?width=100";
 
 function App() {
   return (
     <div>
       <img src={image} />
+      <img src={image100px} />
     </div>
   );
 }
--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <img src="@/assets/Macaca_nigra_self-portrait_large.jpg" />
+    <img src="@/assets/Macaca_nigra_self-portrait_large.jpg?width=100" />
   </div>
 </template>
