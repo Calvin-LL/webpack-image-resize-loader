f33a5c119087f2e13d11599356d5b14e0b4aed6c
Calvin Liang <me@calvin.sh>
2020-12-12 19:50:42 -0800

    test: split tests into e2e and unit

--- test/e2e/helpers/WIRLWebpackTestCompiler.ts
+++ test/e2e/helpers/WIRLWebpackTestCompiler.ts
@@ -14,23 +14,17 @@ interface WIRLCompileOptions extends Omit<CompileOptions, "entryFilePath"> {
 export default class WIRLWebpackTestCompiler extends WebpackTestCompiler {
   compile(options: WIRLCompileOptions = {}): Promise<WebpackTestBundle> {
     const { loaderOptions = {}, entryFileName = "index.js" } = options;
-    const fixturesDir = path.resolve(__dirname, "..", "fixtures");
+    const fixturesDir = path.resolve(__dirname, "../fixtures");
 
     this.webpackConfig = {
       context: fixturesDir,
-      outputPath: path.resolve(__dirname, "..", "outputs"),
+      outputPath: path.resolve(__dirname, "../outputs"),
       rules: [
         {
           test: /(png|jpg|svg)/i,
           rules: [
             {
-              loader: path.resolve(
-                __dirname,
-                "..",
-                "..",
-                "test-dist",
-                "cjs.js"
-              ),
+              loader: path.resolve(__dirname, "../../../test-dist/cjs.js"),
               options: {
                 ...loaderOptions,
                 fileLoaderOptions: {
