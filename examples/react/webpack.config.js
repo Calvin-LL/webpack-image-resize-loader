e049065658f8e026e7d8ca9b878bf8b6a745ee8c
Calvin Liang <me@calvin.sh>
2021-04-06 12:25:25 -0700

    chore(examples): update packages

--- examples/react/webpack.config.js
+++ examples/react/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     path: path.resolve(__dirname, "./build"),
